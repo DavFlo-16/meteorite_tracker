687f147684d7905342c867e0ce48119bc4268316
DavFlo-16 <dvdwdflwrs@gmail.com>
2016-08-22 16:41:34 -0500

    Rename project to meteoriteViewer

--- meteoriteViewer.js
+++ meteoriteViewer.js
@@ -13,7 +13,7 @@
         var state = encodeURIComponent($("#inputState").val().trim());
         var city = encodeURIComponent($("#inputCity").val().trim());
         var zipCode = encodeURIComponent($("#inputZipCode").val().trim());
-        var radius = $("#radius").val()
+        var radiusofInterest = $("#radius").val()
         var address = street+city+state+zipCode;
         console.log(address);
         $.ajax({
@@ -27,11 +27,11 @@
                 console.log(lat + lng);
                 $.ajax({
                   method: 'GET',
-                      url:`https://data.nasa.gov/resource/y77d-th95.json?$where=within_circle(geolocation, ${lat}, ${lng}, ${radius})`,
+                      url:`https://data.nasa.gov/resource/y77d-th95.json?$where=within_circle(geolocation, ${lat}, ${lng}, ${radiusofInterest})`,
                       success: function(NASAdata) {
                         console.log(NASAdata);
                         $('#jQuery').before('<div class="col-sm-8 col-sm-offset-2" id="meteoriteMap"></div>');
-                        createMeteoriteMap(lat, lng);
+                        createMeteoriteMap(lat, lng, radiusofInterest);
                         setMeteoriteMapMarkers(NASAdata);
                       }
                     });
@@ -44,11 +44,42 @@
       //Google Maps API Function Reference
 
       var meteoriteMap;
-      function createMeteoriteMap(latitude, longitude) {
+      function createMeteoriteMap(latitude, longitude, radius) {
         meteoriteMap = new google.maps.Map(document.getElementById('meteoriteMap'), {
           center: {lat: parseFloat(latitude), lng: parseFloat(longitude)},
-          zoom: 4
+          zoom: 4,
+          styles: [
+            {
+              featureType: 'all',
+              stylers: [
+                { saturation: -80 }
+              ]
+            },{
+              featureType: 'road.arterial',
+              elementType: 'geometry',
+              stylers: [
+                { hue: '#00ffee' },
+                { saturation: 50 }
+              ]
+            },{
+              featureType: 'poi.business',
+              elementType: 'labels',
+              stylers: [
+                { visibility: 'off' }
+              ]
+            }
+          ]
         });
+        var meteoriteRadius = new google.maps.Circle({
+          center: {lat: parseFloat(latitude), lng: parseFloat(longitude)},
+          radius: parseFloat(radius),
+          strokeColor:"#0000FF",
+          strokeOpacity:0.8,
+          strokeWeight:2,
+          fillColor:"#0000FF",
+          fillOpacity:0.4
+        });
+        meteoriteRadius.setMap(meteoriteMap);
       }
 
       function createMeteoriteMarker(meteorite){
@@ -60,7 +91,9 @@
         });
 
         marker.setMap(meteoriteMap);
-        infowindow.open(meteoriteMap, marker);
+        marker.addListener('click', function() {
+          infowindow.open(meteoriteMap, marker);
+        });
       }
 
       function setMeteoriteMapMarkers(meteoriteData){
